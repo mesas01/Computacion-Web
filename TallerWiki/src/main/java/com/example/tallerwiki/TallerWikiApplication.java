430d4b2fe10e7cd93b2c03957588a6402d0428ee
Santiago Mesa <mesasantiago5@gmail.com>
2025-08-09 14:47:16 -0500

    configuraciones nuevo proyecto

---
+++ TallerWiki/src/main/java/com/example/tallerwiki/TallerWikiApplication.java
@@ -0,0 +1,13 @@
+package com.example.tallerwiki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TallerWikiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TallerWikiApplication.class, args);
+	}
+
+}
---
+++ TallerWiki/src/test/java/com/example/tallerwiki/TallerWikiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tallerwiki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TallerWikiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
